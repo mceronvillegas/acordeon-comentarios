42195077dac2f93c437fc3ed85247e3f53f116e8
mceronvillegas <1110366789@u.icesi.edu.co>
2025-10-30 20:45:31 -0500

    blah

--- src/reducers/commentsSlice.ts
+++ src/reducers/commentsSlice.ts
@@ -1,41 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 import type { CommentType } from "../types/CommentTypes";
 
 type CommentsState = {
     comments: CommentType[];
-    favorites: number[];
 };
-
-// 🔹 Cargamos favoritos desde LocalStorage si existen
-const initialFavorites = JSON.parse(localStorage.getItem("favorites") || "[]");
 
 const initialState: CommentsState = {
     comments: [],
-    favorites: initialFavorites,
 };
 
 const commentsSlice = createSlice({
     name: "comments",
     initialState,
     reducers: {
-        // Guarda los comentarios obtenidos de la API
-        setComments: (state, action) => {
+        setComments: (state, action: PayloadAction<CommentType[]>) => {
             state.comments = action.payload;
-        },
-
-        // Añade o quita un comentario de los favoritos
-        toggleFavorite: (state, action) => {
-            const id = action.payload;
-            if (state.favorites.includes(id)) {
-                state.favorites = state.favorites.filter((favId) => favId !== id);
-            } else {
-                state.favorites.push(id);
-            }
-            localStorage.setItem("favorites", JSON.stringify(state.favorites));
         },
     },
 });
 
-export const { setComments, toggleFavorite } = commentsSlice.actions;
+export const { setComments } = commentsSlice.actions;
 export default commentsSlice.reducer;
